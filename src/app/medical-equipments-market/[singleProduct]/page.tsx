b6a11a884d3ae503a304fc0f1e105a289e282b76
DanialjDev <danialjdev81@gmail.com>
2023-11-13 00:45:15 +0330

    remove .next.zip file from tracking

--- src/app/medical-equipments-market/[singleProduct]/page.tsx
+++ src/app/medical-equipments-market/[singleProduct]/page.tsx
@@ -1,7 +1,6 @@
 import SingleProductPage from "@/components/pages/medical-equipments-market/SingleProduct";
 import { postPageView } from "@/services/common";
 import { getHomePageDevies } from "@/services/homePage";
-import { getSingleDevice } from "@/services/shop";
 import { headers } from "next/headers";
 import React from "react";
 
